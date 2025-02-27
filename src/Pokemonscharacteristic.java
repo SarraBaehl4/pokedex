df032d4a16aa51f6fcde912dd363687c791c95b3
baehl <baehl44@yahoo.fr>
2025-02-27 10:27:16 +0100

    affichage liste des pokemons pesant plus de 10 kg

--- src/Pokemonscharacteristic.java
+++ src/Pokemonscharacteristic.java
@@ -1,4 +1,5 @@
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class Pokemonscharacteristic {
     public static void main(String[] args) {
@@ -11,5 +12,26 @@ public class Pokemonscharacteristic {
         } else {
             System.out.println("Erreur lors de la récupération des Pokémon.");
         }
+        //Compter et Afficher les pokémons qui pèse plus de 10kg
+            int nbPokemon10Kg = 0;
+            System.out.println("Pokémon de plus de 10kg :");
+            
+            for (int i = 0; i < pokemonList.size(); i++) {
+                JSONObject pokemon = (JSONObject) pokemonList.get(i);
+                String weightStr = (String) pokemon.get("weight");
+                
+                // Extraire la valeur numérique (supprimer "kg")
+                double weight = 0;
+                if (weightStr != null) {
+                    weight = Double.parseDouble(weightStr.replace("kg", "").trim());
+                }
+                
+                if (weight > 10.0) {
+                    nbPokemon10Kg++;
+                    System.out.println(pokemon.get("name") + " - " + weightStr);
+                }
+            }
+            
+            System.out.println("Nombre total des pokémons pesant plus de 10kg: " + nbPokemon10Kg);
     } 
 }
